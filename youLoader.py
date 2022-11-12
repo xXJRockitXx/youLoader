08d0d089d87c34738de5ce2b6d5ea1231b9fa680
JRDesk <luis.alberto.lag21@gmail.com>
2022-11-12 12:04:25 -0600

    Ya funciona, pero falta mejorar

--- youLoader.py
+++ youLoader.py
@@ -5,9 +5,18 @@ from yt_dlp import YoutubeDL
 import tkinter as tk
 from tkinter.filedialog import askdirectory
 
+def app_config(app, width, height):
+    app.geometry("%dx%d" % (width, height))
+    app.configure(background="#404258")
+    tk.Wm.wm_title(app, "YouLoader")
+
+""" We init the app and configure """
 app = tk.Tk()
+width= app.winfo_screenwidth()
+height= app.winfo_screenheight()
+app_config(app, width, height)
+
 link = tk.StringVar()
-filename = ""
 
 class MyLogger(object):
 
@@ -23,37 +32,16 @@ class MyLogger(object):
 
 def my_hook(d):
     if d['status'] == 'finished':
-        print('Descarga completa, convirtiendo ...') 
+        print('Download complete, converting ...') 
 
-
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'writethumbnail': True,
-    'addmetadata':True,
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }, {
-        'key': 'EmbedThumbnail',
-    }, {
-        'key': 'FFmpegMetadata',
-    }],
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
-}
-
-width= app.winfo_screenwidth()
-height= app.winfo_screenheight()
-app.geometry("%dx%d" % (width, height))
-app.configure(background="#404258")
-tk.Wm.wm_title(app, "YouLoader")
 
 def download_directory():
     """ We're gonna "Open" dialog box and return the 
     path to the selected file """
-    """ global filename """
     filename = askdirectory()
+    global path
+    path = filename
+    
     lblPath = tk.Label(app,
                     text="Your song will be here: \n" + filename,
                     textvariable="Your song will be here: \n" + filename,
@@ -69,20 +57,27 @@ def download_directory():
     
     
 def download():
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'writethumbnail': True,
+        'outtmpl': path + '/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
+        'addmetadata':True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }, {
+            'key': 'EmbedThumbnail',
+        }, {
+            'key': 'FFmpegMetadata',
+        }],
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+    }
+    
     """ Starts downloading """
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([link.get()])
-    
-    """ When download finishes, we have to remove the weird characters """
-    files = os.listdir()      
-    
-    for name in files:
-        """ We remove all the characters that come after the [ in 
-        the name of the files """
-        if(name.endswith(".mp3")):
-            top = name.find("[")
-            newName = name[0:top]
-            os.rename(name, newName + ".mp3")
     
     
 """ In this label we're gonna show the instructions """
@@ -95,15 +90,17 @@ lblInstructions = tk.Label(app,
 
 lblInstructions.place(x=20, y=5)
 
+
 """ This entry for the link of the song or playlist """
-lblLink = tk.Entry(app, 
+entLink = tk.Entry(app, 
                     bg="#6B728E",
                     fg="white",
                     relief="flat",
                     textvariable=link,
                     width= 100)
 
-lblLink.place(height=30,x=20, y=width * 0.03)
+entLink.place(height=30,x=20, y=width * 0.03)
+
 
 """ This button for choosing the download directory """
 btnDirectory = tk.Button(app, 
@@ -115,6 +112,7 @@ btnDirectory = tk.Button(app,
           relief="flat")
 btnDirectory.place(x=20, y=width * 0.06)
 
+
 """ This button for start the download """
 btnDownload = tk.Button(app, 
           text="Download", 
@@ -123,6 +121,5 @@ btnDownload = tk.Button(app,
           fg="white",
           command= download,
           relief="flat")
-""" btnDownload.place(x=20, y=width * 0.15) """
 
 app.mainloop()
